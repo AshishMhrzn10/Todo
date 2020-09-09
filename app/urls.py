6e755ffb06eff61976a02dfd428465dd6aaf90b2
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-09 18:28:58 +0545

    Added successful

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name="home"),
+    path('add/', views.add_todo, name="add"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 @login_required(login_url='/accounts/login/')
 def list(request):
-    qs = Todo.objects.all().order_by('-date')
+    qs = Todo.objects.filter(user=request.user).order_by('-date')
     context = {
         'lists': qs
     }
     return render(request, 'app/home.html', context)
 
-
+@login_required(login_url='/accounts/login/')
 def add_todo(request):
-    pass
+    if request.method == "POST":
+        note = request.POST["note"]
+        create = Todo.objects.create(user=request.user, description=note)
+        create.save()
+        messages.success(request, "Created a note")
+        return redirect('home')
+    else:
+        return render(request, 'app/home.html')
+        
+    return render(request, 'app/home.html')
